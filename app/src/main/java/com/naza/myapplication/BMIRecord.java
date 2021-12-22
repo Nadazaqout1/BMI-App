7ea9bb71ed9b532cc5c3434a3427ce6cc7e50904
Nadazaqout1git config --global user.email nadazaqout1@gmail.com <nadazaqout1@gmail.comgit config --global user.name Nadazaqout1>
2021-12-22 17:46:27 +0200

    second

---
+++ app/src/main/java/com/naza/myapplication/BMIRecord.java
@@ -0,0 +1,2 @@
+package com.naza.myapplication;public class BMIRecord {
+}
---
+++ app/src/main/java/com/naza/myapplication/Food_List.java
@@ -0,0 +1,14 @@
+package com.naza.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Food_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_list);
+    }
+}
